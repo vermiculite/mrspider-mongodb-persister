4de85869cab6965d361ab6391c4b9e6f1fa92d62
sean <seanowenhayes@gmail.com>
2015-12-14 18:59:42 +0100

    Stupid tests done, now lets get on with it.

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 module.exports = function(options) {
+    validate(options);
     return function (page, spider, next) {
         next();
     }
 };
+
+function validate (options) {
+    if(!options) {
+        throw new Error('No options passed to mrspider mongodb persister');
+    }
+    if(!options.db) {
+        throw new Error('No db property passed to mrspider mongodb persister');
+    }
+    if(!options.db.url) {
+        throw new Error('No db.url property passed to mrspider mongodb persister');
+    }
+    if(!options.db.collection) {
+        throw new Error('No db.collection property passed to mrspider mongodb persister');
+    }
+}
--- test/index.js
+++ test/index.js
@@ -1,21 +1,61 @@
 var mongodbPersister = require('..');
 
+var chai = require('chai');
+var should = chai.should();
+
 describe('mrspider mongodb persister', function () {
 
     var validPage;
     var validSpider;
     var validNext;
+    var validParams;
 
 
     beforeEach(function () {
         validPage = {};
         validSpider = {};
         validNext = {};
+        validParams = {
+            db: {
+                url: 'mongodb://localhost:27017/myproject',
+                options: {},
+                collection: 'crawlresults'
+            }
+        };
     });
 
 
     it('should call next', function (done) {
-        var persister = mongodbPersister();
+        var persister = mongodbPersister(validParams);
         persister(validPage, validSpider, done);
     });
+
+    it('should throw an error given no options', function() {
+        (function() {
+            var persister = mongodbPersister();
+        }).should.throw(Error);
+    });
+
+    it('should throw an error given no db property in the options', function() {
+        delete validParams.db;
+        (function() {
+            var persister = mongodbPersister(validParams);
+        }).should.throw(Error);
+    });
+
+    it('should throw an error given no url db property in the options', function() {
+        delete validParams.db.url;
+        (function() {
+            var persister = mongodbPersister(validParams);
+        }).should.throw(Error);
+    });
+
+    it('should throw an error given no collection db property in the options', function() {
+        delete validParams.db.collection;
+        (function() {
+            var persister = mongodbPersister(validParams);
+        }).should.throw(Error);
+    });
+
+
 });
