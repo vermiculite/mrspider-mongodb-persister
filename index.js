03f5c2328e00431417f2ad36d18ba44e10af618e
sean <seanowenhayes@gmail.com>
2016-01-04 09:40:57 +0100

    Fixed bug not calling next on invalid pages.

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ module.exports = function(options) {
     return function (page, spider, next) {
         if(page.valid) {
             MongoClient.connect(options.url, function(err, db) {
+                if(err) {
+                    console.err('could not connect to the database at %s', options.url);
+                    return next();
+                }
                 db.collection(option.collection).insertOne(page.data, function(err, result) {
                     db.close();
                     next();
                 });
             });
+        } else {
+            next();
         }
-
-
     }
 };
 
